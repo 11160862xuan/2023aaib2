87fc75815133fdd0e59b362424dec1f7d1cac85c
11160862xuan <114331586+11160862xuan@users.noreply.github.com>
2024-05-17 11:46:31 +0800

    week13

---
+++ week13/week13-3.cpp
@@ -0,0 +1,25 @@
+///week13-3.cpp剝皮法，也要做迴文判斷
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int N=0;
+    int a[20]={};
+    while(n>0){
+        a[N++]=n%10;///int now=n%10;
+        ///printf("now:%d\n",now);
+        n= n/10;
+
+    }
+    int bad=0;
+    for(int i=0;i<N;i++){
+        if(a[i]!=a[N-1-i])bad=1;
+
+
+    }
+    if(bad==1)printf("NO\n");
+    else printf("Yes\n");
+
+
+}
