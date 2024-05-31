fd346fafc0d7748813d62ef5ba6c9f8fe9862546
11160862xuan <114331586+11160862xuan@users.noreply.github.com>
2024-05-31 11:41:12 +0800

    week15

---
+++ week15/week15-4.cpp
@@ -0,0 +1,11 @@
+///week15-4.cpp想了解字母距離
+#include<stdio.h>
+int main()
+{
+    if('A'==65)printf("字母A竟然和65相等\n");
+    printf("字母%的值是%d\n",65,'A');
+    printf("字母%的值是%d\n",66,'B');
+    printf("字母%的值是%d\n",67,'C');
+    printf("\n");
+    printf("字母%的值是%d\n",97,'a');
+}
